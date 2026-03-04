3ca22a23ca34d21ccebb2f986776edf8c26368af
rchelisg <jssl68@gmail.com>
2026-03-05 01:26:33 +0800

    这里要填什么？

---
+++ service-worker.js
@@ -0,0 +1,71 @@
+// 缓存名称
+const CACHE_NAME = 'battery-calculator-v1';
+
+// 需要缓存的资源
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon.svg'
+];
+
+// 安装 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活 Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有响应，则返回缓存的响应
+        if (response) {
+          return response;
+        }
+        
+        // 否则，发起网络请求
+        return fetch(event.request).then(
+          response => {
+            // 检查响应是否有效
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // 克隆响应
+            const responseToCache = response.clone();
+            
+            // 将响应添加到缓存
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          }
+        );
+      })
+  );
+});
